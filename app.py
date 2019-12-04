92f54e37d56684951d1284a2361a076f28aa5643
aaselkov <alex.de.grey@yandex.ru>
2019-12-04 20:18:19 +0500

    Entering the message for the first commit!

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
 
+from routes import api
+
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return 'Hello, world!'
+app.register_blueprint(api)
 
 
 if __name__ == '__main__':
