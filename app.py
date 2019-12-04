b7beb89528490d5d335ed73c2101765533d4c2c4
aaselkov <alex.de.grey@yandex.ru>
2019-12-04 21:22:22 +0500

    New instance of Client added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from models import db, Men
+from models import db, Man, Client
 from routes import api
 
 app = Flask(__name__)
@@ -8,8 +8,9 @@ app.register_blueprint(api)
 db.init_app(app)
 with app.app_context():
     db.create_all()
-    db.session.add(Men(name='Bob'))
-    db.session.add(Men(name='Rocky'))
+    db.session.add(Man(name='Bob'))
+    db.session.add(Man(name='Rocky'))
+    db.session.add(Client(name='Client 1', clientType='New', phone='2292929'))
     db.session.commit()
 
 if __name__ == '__main__':
