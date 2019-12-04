d407ace146f1553bab9338e69e1aee295ee17298
aaselkov <alex.de.grey@yandex.ru>
2019-12-04 20:59:55 +0500

    db session commit

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 
-from models import db
+from models import db, Men
 from routes import api
 
 app = Flask(__name__)
 app.register_blueprint(api)
 db.init_app(app)
+with app.app_context():
+    db.create_all()
+    db.session.add(Men(name='Bob'))
+    db.session.add(Men(name='Rocky'))
+    db.session.commit()
 
 if __name__ == '__main__':
     app.run()
