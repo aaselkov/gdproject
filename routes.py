c31f0b59fade3942dbc36f1c181d28a5b3a52fca
aaselkov <alex.de.grey@yandex.ru>
2019-12-04 21:22:52 +0500

    New routes to class instances Clients added

--- routes.py
+++ routes.py
@@ -1,10 +1,29 @@
 from flask import Blueprint, jsonify
 
-from models import Men
+from models import Man, db, Client
 
-api = Blueprint('api',__name__)
+api = Blueprint('api',__name__, url_prefix='/api')
 
 
-@api.route('/')
-def hello():
-    return jsonify([(lambda men: men.json())(men) for men in Men.query.all()])
+@api.route('/people')
+def get_people():
+    return jsonify([(lambda man: man.json())(man) for man in Man.query.all()])
+
+
+@api.route('/man/id/<int:man_id>')
+def get_man(man_id):
+    man = Man.query.get(man_id)
+    return jsonify(man.json()) if man else ''
+
+
+@api.route('/man/name/<int:man_id>,<string:man_name>')
+def put_man(man_id, man_name):
+    man = Man(id=man_id, name=man_name)
+    db.session.add(man)
+    db.session.commit()
+    return 'done'
+
+
+@api.route('/clients')
+def get_clients():
+    return jsonify([(lambda client: client.json())(client) for client in Client.query.all()])
