bb5c4de5ea19c40f0bb6b44e7413263e100cd8cb
aaselkov <alex.de.grey@yandex.ru>
2019-12-04 20:36:27 +0500

    Entering the message for the first commit!

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
+from models import db
 from routes import api
 
 app = Flask(__name__)
 app.register_blueprint(api)
-
+db.init_app(app)
 
 if __name__ == '__main__':
     app.run()
