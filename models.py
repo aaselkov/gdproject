0c2e1f909b74c0ba5ba09ed4c319fcac6b2b72b9
aaselkov <alex.de.grey@yandex.ru>
2019-12-04 21:22:34 +0500

    New class Client added

--- models.py
+++ models.py
@@ -3,10 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Men(db.Model):
+class Man(db.Model):
     __tablename__ = 'people'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
 
     def json(self):
         return {'id': self.id, 'name': self.name}
+class Client(Man):
+    __tablename__ = 'clients'
+    clientType = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+
+    def json(self):
+        return {'id': self.id, 'name': self.name,'client type': self.clientType, 'phone': self.phone}
