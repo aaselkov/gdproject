e6f12a57e28e7b3d68d33064b0b76d71488de15a
aaselkov <alex.de.grey@yandex.ru>
2019-12-05 19:05:06 +0500

    Entering the message for the first commit!

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ with app.app_context():
     db.session.add(Man(name='Bob'))
     db.session.add(Man(name='Rocky'))
     db.session.add(Client(name='Client 1', clientType='New', phone='2292929'))
+    db.session.add(Client(name='Client 2', clientType='Existing', phone='2282828'))
+    db.session.add(Client(name='Client 3', clientType='Existing', phone='2757473'))
     db.session.commit()
 
 if __name__ == '__main__':
