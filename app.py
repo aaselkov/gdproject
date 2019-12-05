76aa0598e5c0908ef4f9076fbca400081e9db2b5
aaselkov <alex.de.grey@yandex.ru>
2019-12-05 20:35:25 +0500

    Entering the message for the first commit!

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from models import db, Client, Type
+from models import db, Client, Type, Treaty
 from routes import api, index
 
 app = Flask(__name__)
@@ -16,9 +16,19 @@ with app.app_context():
     db.session.add(existing)
     db.session.add(new)
     db.session.commit()
-    db.session.add(Client(name='Client 1', clientTypeId=existing.id, phone='2292929'))
-    db.session.add(Client(name='Client 2', clientTypeId=existing.id, phone='2282828'))
-    db.session.add(Client(name='Client 3', clientTypeId=new.id, phone='2757473'))
+    cli1 = Client(name='Client 1', clientTypeId=existing.id, phone='2292929')
+    cli2 = Client(name='Client 2', clientTypeId=existing.id, phone='2282828')
+    cli3 = Client(name='Client 3', clientTypeId=new.id, phone='2757473')
+    db.session.add(cli1)
+    db.session.add(cli2)
+    db.session.add(cli3)
+    db.session.commit()
+    dog1 = Treaty(name='Dogovor 1', clientId=cli1.id)
+    dog2 = Treaty(name='Dogovor 2', clientId=cli2.id)
+    dog3 = Treaty(name='Dogovor 3', clientId=cli3.id)
+    db.session.add(dog1)
+    db.session.add(dog2)
+    db.session.add(dog3)
     db.session.commit()
 
 if __name__ == '__main__':
