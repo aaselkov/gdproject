69bc742d81bc177e5358953fdb225ecc3c780d06
aaselkov <alex.de.grey@yandex.ru>
2019-12-05 20:14:06 +0500

    Entering the message for the first commit!

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from models import db, Man, Client
+from models import db, Client, Type
 from routes import api, index
 
 app = Flask(__name__)
@@ -9,11 +9,16 @@ app.register_blueprint(index)
 db.init_app(app)
 with app.app_context():
     db.create_all()
-    db.session.add(Man(name='Bob'))
-    db.session.add(Man(name='Rocky'))
-    db.session.add(Client(name='Client 1', clientType='New', phone='2292929'))
-    db.session.add(Client(name='Client 2', clientType='Existing', phone='2282828'))
-    db.session.add(Client(name='Client 3', clientType='Existing', phone='2757473'))
+    # db.session.add(Man(name='Bob'))
+    # db.session.add(Man(name='Rocky'))
+    existing = Type(name='Existing')
+    new = Type(name='New')
+    db.session.add(existing)
+    db.session.add(new)
+    db.session.commit()
+    db.session.add(Client(name='Client 1', clientTypeId=existing.id, phone='2292929'))
+    db.session.add(Client(name='Client 2', clientTypeId=existing.id, phone='2282828'))
+    db.session.add(Client(name='Client 3', clientTypeId=new.id, phone='2757473'))
     db.session.commit()
 
 if __name__ == '__main__':
