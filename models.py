e9d3a111ce1ef1ce51f46cc776c655fd04746345
aaselkov <alex.de.grey@yandex.ru>
2019-12-05 19:05:11 +0500

    Entering the message for the first commit!

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -10,6 +12,8 @@ class Man(db.Model):
 
     def json(self):
         return {'id': self.id, 'name': self.name}
+
+
 class Client(Man):
     __tablename__ = 'clients'
     clientType = db.Column(db.String(120))
@@ -17,3 +21,15 @@ class Client(Man):
 
     def json(self):
         return {'id': self.id, 'name': self.name,'client type': self.clientType, 'phone': self.phone}
+
+
+class Treaty(db.Model):
+    __tablename__ = 'treaty'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    client = db.Column(db.Integer)
+
+    def json(self):
+        return {'id': self.id, 'name': self.name,'date': self.date, 'client': self.client}
+
